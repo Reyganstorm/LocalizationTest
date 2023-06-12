d63f0c5c08655b229be3681911a680dc94e40b7b
Ruslan Shtybaev <79254522+Reyganstorm@users.noreply.github.com>
2023-06-12 11:04:00 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LocalizationTest
+
+A description of this package.
---
+++ Tests/LocalizationTestTests/LocalizationTestTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LocalizationTest
+
+final class LocalizationTestTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LocalizationTest().text, "Hello, World!")
+    }
+}
